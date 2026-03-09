b2950321548a4cf518bb55f80dfea1d40bb69fa8
Nino <ninosim@gmail.com>
2026-03-09 16:17:57 -0300

    Springboot Framework

---
+++ src/main/java/com/uniesp/tech/SistemaUniespApplication.java
@@ -0,0 +1,13 @@
+package com.uniesp.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaUniespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaUniespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniesp/tech/SistemaUniespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniesp.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaUniespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
